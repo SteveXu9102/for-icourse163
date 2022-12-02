d344533bd3ac4de450b5f6f2a75c2c14cfc2aa1a
XU YANGYANG <68725147+SteveXu9102@users.noreply.github.com>
2022-12-02 09:19:48 +0800

    Added OJ 7-2

---
+++ C Programming/7-2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <malloc.h>
+
+void sort(int* a, int n);
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	
+	int* array = (int*)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &array[i]);
+	}
+
+	sort(array, n);
+	
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d  ", array[i]);
+	}
+
+	printf("\n");
+	return 0;
+}
+
+void sort(int* a, int n)
+{
+	int mid;
+	for (int i = 0; i < n-1; i++)
+	{
+		for (int j = i+1; j < n; j = j++)
+		{
+			if (*(a+i) >= *(a+j))
+			{
+				mid = *(a + i);
+				*(a + i) = *(a + j);
+				*(a + j) = mid;
+			}
+		}
+	}
+}
