31184f40fa95d006bf3ad2841ef054fcefc00db7
XU YANGYANG <68725147+SteveXu9102@users.noreply.github.com>
2022-12-17 18:47:20 +0800

    10.c improvement

--- C Programming/10.c
+++ C Programming/10.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define DEBUG   // [ATTENTION] Added for data integrity check. REMOVE THIS ROW BEFORE SUBMISSION OR GET UNACCEPTED.
+#define DEBUG   // [ATTENTION] Added for data integrity check. REMOVE THIS ROW BEFORE SUBMISSION.
 
 typedef struct Scores {
     int advmath;
@@ -20,13 +20,16 @@ int main() {
     scanf("%d",&i);
 
     for (int j=0; j < i; ++j) {
-        scanf("%ld %s %d %d %d",&students[j].idnum,&students[j].name,&students[j].scores.advmath,&students[j].scores.physics,&students[j].scores.english);
+        scanf("%ld %s %d %d %d",&students[j].idnum,students[j].name,&students[j].scores.advmath,&students[j].scores.physics,&students[j].scores.english);
         students[j].scores.average=(students[j].scores.advmath+students[j].scores.physics+students[j].scores.english)/3;
     }
 
     for (int j = 0; j < i; ++j) {
-#ifdef DEBUG   // [INFO] Added for data integrity check, Whether remove row 28-30 or not is up to you.
-        printf("%dth student = %s, ID number = %ld\n",j+1,students[j].name,students[j].idnum);
+#ifdef DEBUG   // [INFO] Added for data integrity check.
+       if (j == 0) printf("%dst student = %s, ID number = %ld\n",j+1,students[j].name,students[j].idnum);
+        else if (j == 1) printf("%dnd student = %s, ID number = %ld\n",j+1,students[j].name,students[j].idnum);
+        else if (j == 2) printf("%drd student = %s, ID number = %ld\n",j+1,students[j].name,students[j].idnum);
+        else printf("%dth student = %s, ID number = %ld\n",j+1,students[j].name,students[j].idnum);
 #endif
         printf("The average score of the %dth student is %d.\n",j+1,students[j].scores.average);
     }
